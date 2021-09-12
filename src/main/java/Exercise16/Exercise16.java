46b3e55aac7172c76209e8a1da764340f0f0465a
itsbot <patrickiannini@knights.ucf.edu>
2021-09-12 14:19:03 -0400

    Completed Exercise

--- src/main/java/Exercise16/Exercise16.java
+++ src/main/java/Exercise16/Exercise16.java
@@ -3,9 +3,16 @@
  *  Copyright 2021 Patrick Iannini
  */
 package Exercise16;
+import java.util.Scanner;
 
 public class Exercise16 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your age?");
+        int age = sc.nextInt();
+
+        String canDrive = (age>=16) ? (" ") : (" not ");
+
+        System.out.println("You are"+canDrive+"old enough to legally drive.");
     }
 }
